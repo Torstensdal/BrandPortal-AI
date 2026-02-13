e88ee58c2459afb624793c487c52c7adab36d24c
Torstensdal <69104886+Torstensdal@users.noreply.github.com>
2026-02-13 09:22:56 +0100

    Emergency Deploy: 09:22:56

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+By Lou
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    // Dette gør process.env.API_KEY tilgængelig i koden på klientsiden
-    'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
-  },
-  server: {
-    port: 5173,
-    strictPort: true,
-    host: '0.0.0.0'
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild'
-  }
-});
+Nøkkerosevej 3
+8240 Risskov
+tandsmykker@gmail.com
+Cvr. Nr.: 44228173
