3ad683919a34d08739198205fa5ba86929da1e13
Torstensdal <69104886+Torstensdal@users.noreply.github.com>
2026-02-14 16:05:29 +0100

    Fix TypeScript configuration and dependencies

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App';
-import { LanguageProvider } from '@/context/LanguageContext';
-import './index.css';
-const container = document.getElementById('root');
-if (container) { createRoot(container).render(<React.StrictMode><LanguageProvider><App /></LanguageProvider></React.StrictMode>); }
